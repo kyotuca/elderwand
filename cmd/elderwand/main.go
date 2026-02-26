83da31c09da213301a762271940c4f4a3086e9b9
Francesco Montefusco <francesco.montefusco@mail.polimi.it>
2026-02-26 13:24:38 +0100

    feat: setup http

--- cmd/elderwand/main.go
+++ cmd/elderwand/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"text/template"
+	"html/template"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type Page struct {
-	Title string,
-	Body []byte,
+	Title  string
+	Body   []byte
 	Player string
 }
 
-func loadPage() (*Page) {
+type Router struct {
+	render func(http.ResponseWriter, string, any)
+}
+
+func loadPage() *Page {
+	body := []byte{0}
+	return &Page{Title: "foo", Body: body, Player: "bar"}
+}
+
+func processTemplate() *template.Template {
+	return template.Must(template.New("").Funcs(template.FuncMap{
+		"upper": strings.ToUpper,
+		"truncate": func(s string, n int) string {
+			if len(s) <= n {
+				return s
+			}
+			return s[:n] + "..."
+		},
+		"fmtDate": func(t time.Time) string {
+			return t.Format("01 Jan 1970")
+		},
+	}).ParseGlob("views/**/*.html"))
 }
